cc8217b4fb387a4705686438d756fcbd9762fa60
newjbl <newjbl@163.com>
2026-02-15 19:16:13 +0800

    updage login and setting

--- ue/connect_home.gd
+++ ue/connect_home.gd
@@ -15,9 +15,13 @@ var SORT_METHOD:String = 'NAME_AZ'# NAME_ZA, TIME_AZ, TIME_ZA, SIZE_AZ, SIZE_ZA
 var UE_SAVE_TIME:int = 30
 var DIS_FILE_TYPE:Array = ['zip']
 var label_setting_font_10:LabelSettings = null
+var label_setting_font_red:LabelSettings = null
+var label_setting_font_blue:LabelSettings = null
 
 var win_size:Vector2i = Vector2i.ZERO
 var hbox_l1:HBoxContainer = null
+var vbox_l1_1_login:VBoxContainer = null
+var vbox_l1_2_setting:VBoxContainer = null
 var hbox_l2:HBoxContainer = null
 var vbox_l3:VBoxContainer = null
 var vbox_l3_vbox:VBoxContainer = null
@@ -51,6 +55,10 @@ var upload_finish:bool = false
 func _ready() -> void:
 	label_setting_font_10 = LabelSettings.new()
 	label_setting_font_10.font_size = 10
+	label_setting_font_red = LabelSettings.new()
+	label_setting_font_red.font_color = Color(1.0, 0.0, 0.0, 1.0)
+	label_setting_font_blue = LabelSettings.new()
+	label_setting_font_blue.font_color = Color(0.0, 1.0, 0.0, 1.0)
 	
 	load_cfg()
 	print(UE_ROOT_DIR, SERVER_IP, UPLOAD_PORT, DOWNLOAD_PORT)
@@ -73,19 +81,31 @@ func build_gui() -> void:
 	vbox_top.size = win_size
 	
 	hbox_l1 = HBoxContainer.new()
-	hbox_l1.size = Vector2i(win_size.x, 40)
+	hbox_l1.size = Vector2i(win_size.x - 5, 40)
 	hbox_l1.name = 'L1'
 	
+	vbox_l1_1_login = VBoxContainer.new()
+	vbox_l1_1_login.size = Vector2i(win_size.x - 5, 300)
+	vbox_l1_1_login.visible = false
+	vbox_l1_1_login.name = 'vbox_l1_1_login'
+	
+	vbox_l1_2_setting = VBoxContainer.new()
+	vbox_l1_2_setting.size = Vector2i(win_size.x - 5, 300)
+	vbox_l1_2_setting.visible = false
+	vbox_l1_2_setting.name = 'vbox_l1_2_setting'
+	
 	hbox_l2 = HBoxContainer.new()
-	hbox_l2.size = Vector2i(win_size.x, 40)
+	hbox_l2.size = Vector2i(win_size.x - 5, 40)
 	hbox_l2.name = 'L2'
 	
 	vbox_l3 = VBoxContainer.new()
-	vbox_l3.size = Vector2i(win_size.x, win_size.y - 100 - 40 - 40)
+	vbox_l3.size = Vector2i(win_size.x - 10, win_size.y - 100 - 40 - 40)
 	vbox_l3.name = 'L3'
 	
 	add_child(vbox_top)
 	vbox_top.add_child(hbox_l1)
+	vbox_top.add_child(vbox_l1_1_login)
+	vbox_top.add_child(vbox_l1_2_setting)
 	vbox_top.add_child(hbox_l2)
 	vbox_top.add_child(vbox_l3)
 	
@@ -93,6 +113,7 @@ func build_gui() -> void:
 	var login_bt:Button = Button.new()
 	login_bt.text = '登录'
 	login_bt.name = 'login_bt'
+	login_bt.connect("pressed", _on_login_bt_pressed)
 	hbox_l1.add_child(login_bt)
 	var sp:Control = Control.new()
 	sp.size_flags_horizontal = Control.SIZE_EXPAND_FILL
@@ -101,8 +122,276 @@ func build_gui() -> void:
 	var setting_bt:Button = Button.new()
 	setting_bt.text = '···'
 	setting_bt.name = 'setting'
+	setting_bt.connect("pressed", _on_setting_bt_pressed)
 	hbox_l1.add_child(setting_bt)
 	
+	### l1 login
+	var hbox_login_l1:HBoxContainer = HBoxContainer.new()
+	var hbox_login_l2:HBoxContainer = HBoxContainer.new()
+	var hbox_login_l3:HBoxContainer = HBoxContainer.new()
+	var hbox_login_l4:HBoxContainer = HBoxContainer.new()
+	var hbox_login_l5:HBoxContainer = HBoxContainer.new()
+	var hbox_login_l6:HBoxContainer = HBoxContainer.new()
+	var hbox_login_l7:HBoxContainer = HBoxContainer.new()
+	var hbox_login_le:HBoxContainer = HBoxContainer.new()
+	hbox_login_l1.name = 'hbox_login_l1'
+	hbox_login_l2.name = 'hbox_login_l2'
+	hbox_login_l3.name = 'hbox_login_l3'
+	hbox_login_l4.name = 'hbox_login_l4'
+	hbox_login_l5.name = 'hbox_login_l5'
+	hbox_login_l6.name = 'hbox_login_l6'
+	hbox_login_l7.name = 'hbox_login_l7'
+	hbox_login_le.name = 'hbox_login_le'
+	vbox_l1_1_login.add_child(hbox_login_l1)
+	vbox_l1_1_login.add_child(hbox_login_l2)
+	vbox_l1_1_login.add_child(hbox_login_l3)
+	vbox_l1_1_login.add_child(hbox_login_l4)
+	vbox_l1_1_login.add_child(hbox_login_l5)
+	vbox_l1_1_login.add_child(hbox_login_l6)
+	vbox_l1_1_login.add_child(hbox_login_l7)
+	vbox_l1_1_login.add_child(hbox_login_le)
+	
+	var login_title_label:Label = Label.new()
+	login_title_label.name = 'login_title_label'
+	login_title_label.text = '登录信息'
+	hbox_login_l1.add_child(login_title_label)
+	
+	var server_ip_label:Label = Label.new()
+	server_ip_label.name = 'server_ip_label'
+	server_ip_label.text = '服务器IP:'
+	hbox_login_l2.add_child(server_ip_label)
+	var server_ip_input:LineEdit = LineEdit.new()
+	server_ip_input.name = 'server_ip_label'
+	server_ip_input.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	hbox_login_l2.add_child(server_ip_input)
+	
+	var upload_port_label:Label = Label.new()
+	upload_port_label.name = 'upload_port_label'
+	upload_port_label.text = '上传端口:'
+	hbox_login_l3.add_child(upload_port_label)
+	var upload_port_input:LineEdit = LineEdit.new()
+	upload_port_input.name = 'upload_port_input'
+	upload_port_input.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	hbox_login_l3.add_child(upload_port_input)
+	
+	var download_port_label:Label = Label.new()
+	download_port_label.name = 'download_port_label'
+	download_port_label.text = '下载端口:'
+	hbox_login_l4.add_child(download_port_label)
+	var download_port_input:LineEdit = LineEdit.new()
+	download_port_input.name = 'download_port_input'
+	download_port_input.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	hbox_login_l4.add_child(download_port_input)
+	
+	var usr_label:Label = Label.new()
+	usr_label.name = 'usr_label'
+	usr_label.text = '用户名:'
+	hbox_login_l5.add_child(usr_label)
+	var usr_input:LineEdit = LineEdit.new()
+	usr_input.name = 'usr_input'
+	usr_input.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	hbox_login_l5.add_child(usr_input)
+	
+	var psd_label:Label = Label.new()
+	psd_label.name = 'psd_label'
+	psd_label.text = '密码:'
+	hbox_login_l6.add_child(psd_label)
+	var psd_input:LineEdit = LineEdit.new()
+	psd_input.name = 'psd_input'
+	psd_input.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	hbox_login_l6.add_child(psd_input)
+	
+	var save_cfg_bt:Button = Button.new()
+	save_cfg_bt.name = 'save_cfg_label'
+	save_cfg_bt.text = '保存配置'
+	save_cfg_bt.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	save_cfg_bt.connect("pressed", _on_save_cfg_bt_pressed.bind(login_title_label, save_cfg_bt,
+	server_ip_input, upload_port_input, download_port_input, usr_input, psd_input))
+	hbox_login_l7.add_child(save_cfg_bt)
+	var test_bt:Button = Button.new()
+	test_bt.name = 'test_bt'
+	test_bt.text = '测试连接'
+	test_bt.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	test_bt.connect("pressed", _on_test_bt_pressed.bind(login_title_label, test_bt,
+	server_ip_input, upload_port_input, download_port_input, usr_input, psd_input))
+	hbox_login_l7.add_child(test_bt)
+	
+	var line1:Line2D = Line2D.new()
+	line1.name = 'line1'
+	line1.add_point(Vector2i(0, 0))
+	line1.add_point(Vector2i(win_size.x, 0))
+	hbox_login_le.add_child(line1)
+	
+	### l2 setting
+	var hbox_setting_l1:HBoxContainer = HBoxContainer.new()
+	var hbox_setting_l2:HBoxContainer = HBoxContainer.new()
+	var hbox_setting_l3:HBoxContainer = HBoxContainer.new()
+	var hbox_setting_l4:HBoxContainer = HBoxContainer.new()
+	var hbox_setting_l5:HBoxContainer = HBoxContainer.new()
+	var hbox_setting_l6:HBoxContainer = HBoxContainer.new()
+	var hbox_setting_l7:HBoxContainer = HBoxContainer.new()
+	var hbox_setting_le:HBoxContainer = HBoxContainer.new()
+	hbox_setting_l1.name = 'hbox_setting_l1'
+	hbox_setting_l2.name = 'hbox_setting_l2'
+	hbox_setting_l3.name = 'hbox_setting_l3'
+	hbox_setting_l4.name = 'hbox_setting_l4'
+	hbox_setting_l5.name = 'hbox_setting_l5'
+	hbox_setting_l6.name = 'hbox_setting_l6'
+	hbox_setting_l7.name = 'hbox_setting_l7'
+	hbox_setting_le.name = 'hbox_setting_le'
+	vbox_l1_2_setting.add_child(hbox_setting_l1)
+	vbox_l1_2_setting.add_child(hbox_setting_l2)
+	vbox_l1_2_setting.add_child(hbox_setting_l3)
+	vbox_l1_2_setting.add_child(hbox_setting_l4)
+	vbox_l1_2_setting.add_child(hbox_setting_l5)
+	vbox_l1_2_setting.add_child(hbox_setting_l6)
+	vbox_l1_2_setting.add_child(hbox_setting_l7)
+	vbox_l1_2_setting.add_child(hbox_setting_le)
+	
+	var setting_save_bt:Button = Button.new()
+	setting_save_bt.name = 'setting_save_bt'
+	setting_save_bt.text = '保存配置'
+	setting_save_bt.add_theme_font_size_override('font_size', 10)
+	setting_save_bt.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	#setting_save_bt.connect("pressed", _on_save_cfg_bt_pressed.bind(login_title_label, save_cfg_bt,
+	#server_ip_input, upload_port_input, download_port_input, usr_input, psd_input))
+	hbox_setting_l1.add_child(setting_save_bt)
+	
+	var dis_size_label:Label = Label.new()
+	dis_size_label.name = 'dis_size_label'
+	dis_size_label.text = '显示粒度:'
+	hbox_setting_l2.add_child(dis_size_label)
+	var radio_group:ButtonGroup = ButtonGroup.new()
+	var radio_day:CheckBox = CheckBox.new()
+	var radio_week:CheckBox = CheckBox.new()
+	var radio_month:CheckBox = CheckBox.new()
+	radio_day.button_group = radio_group
+	radio_week.button_group = radio_group
+	radio_month.button_group = radio_group
+	radio_day.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	radio_week.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	radio_month.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	radio_day.name = 'DAY'
+	radio_day.text = '日'
+	radio_week.name = 'WEEK'
+	radio_week.text = '周'
+	radio_month.name = 'MONTH'
+	radio_month.text = '月'
+	radio_day.add_theme_font_size_override("font_size", 10)
+	radio_week.add_theme_font_size_override("font_size", 10)
+	radio_month.add_theme_font_size_override("font_size", 10)
+	hbox_setting_l2.add_child(radio_day)
+	hbox_setting_l2.add_child(radio_week)
+	hbox_setting_l2.add_child(radio_month)
+	
+	var vbox_time:VBoxContainer = VBoxContainer.new()
+	var hbox_time_l1:HBoxContainer = HBoxContainer.new()
+	var hbox_time_l2:HBoxContainer = HBoxContainer.new()
+	var duration_label:Label = Label.new()
+	duration_label.name = 'duration_label'
+	duration_label.text = '时间范围'
+	duration_label.label_settings = label_setting_font_10
+	hbox_setting_l3.add_child(duration_label)
+	var y1:OptionButton = OptionButton.new()
+	var m1:OptionButton = OptionButton.new()
+	var d1:OptionButton = OptionButton.new()
+	var y2:OptionButton = OptionButton.new()
+	var m2:OptionButton = OptionButton.new()
+	var d2:OptionButton = OptionButton.new()
+	y1.name = 'y1'
+	m1.name = 'm1'
+	d1.name = 'd1'
+	y2.name = 'y2'
+	m2.name = 'm2'
+	d2.name = 'd2'
+	hbox_time_l1.add_child(y1)
+	hbox_time_l1.add_child(m1)
+	hbox_time_l1.add_child(d1)
+	hbox_time_l2.add_child(y2)
+	hbox_time_l2.add_child(m2)
+	hbox_time_l2.add_child(d2)
+	vbox_time.add_child(hbox_time_l1)
+	vbox_time.add_child(hbox_time_l2)
+	hbox_setting_l3.add_child(vbox_time)
+	
+	var sort_method_label:Label = Label.new()
+	var sort_p1:VBoxContainer = VBoxContainer.new()
+	var sort_p2:VBoxContainer = VBoxContainer.new()
+	var sort_p3:VBoxContainer = VBoxContainer.new()
+	sort_method_label.name = 'sort_method_label'
+	sort_method_label.text = '排序方式'
+	sort_method_label.label_settings = label_setting_font_10
+	hbox_setting_l4.add_child(sort_method_label)
+	var radio_group_1:ButtonGroup = ButtonGroup.new()
+	var nameaz_bt:CheckBox = CheckBox.new()
+	var nameza_bt:CheckBox = CheckBox.new()
+	var timeaz_bt:CheckBox = CheckBox.new()
+	var timeza_bt:CheckBox = CheckBox.new()
+	var sizeaz_bt:CheckBox = CheckBox.new()
+	var sizeza_bt:CheckBox = CheckBox.new()
+	nameaz_bt.button_group = radio_group_1
+	nameza_bt.button_group = radio_group_1
+	timeaz_bt.button_group = radio_group_1
+	timeza_bt.button_group = radio_group_1
+	sizeaz_bt.button_group = radio_group_1
+	sizeza_bt.button_group = radio_group_1
+	nameaz_bt.text = '名字\n顺序'
+	nameaz_bt.name = 'nameaz_bt'
+	nameza_bt.text = '名字\n倒序'
+	nameza_bt.name = 'nameza_bt'
+	timeaz_bt.text = '时间\n顺序'
+	timeaz_bt.name = 'timeaz_bt'
+	timeza_bt.text = '时间\n倒序'
+	timeza_bt.name = 'timeza_bt'
+	sizeaz_bt.text = '大小\n顺序'
+	sizeaz_bt.name = 'sizeaz_bt'
+	sizeza_bt.text = '大小\n逆序'
+	sizeza_bt.name = 'sizeza_bt'
+	nameaz_bt.add_theme_font_size_override('font_size', 10)
+	nameza_bt.add_theme_font_size_override('font_size', 10)
+	timeaz_bt.add_theme_font_size_override('font_size', 10)
+	timeza_bt.add_theme_font_size_override('font_size', 10)
+	sizeaz_bt.add_theme_font_size_override('font_size', 10)
+	sizeza_bt.add_theme_font_size_override('font_size', 10)
+	sort_p1.add_child(nameaz_bt)
+	sort_p1.add_child(nameza_bt)
+	sort_p2.add_child(timeaz_bt)
+	sort_p2.add_child(timeza_bt)
+	sort_p3.add_child(sizeaz_bt)
+	sort_p3.add_child(sizeza_bt)
+	hbox_setting_l4.add_child(sort_p1)
+	hbox_setting_l4.add_child(sort_p2)
+	hbox_setting_l4.add_child(sort_p3)
+	
+	var ue_save_duration:Label = Label.new()
+	ue_save_duration.name = 'ue_save_duration'
+	ue_save_duration.text = '手机存储天数:'
+	ue_save_duration.add_theme_font_size_override('font_size', 10)
+	hbox_setting_l5.add_child(ue_save_duration)
+	var ue_save_input:LineEdit = LineEdit.new()
+	ue_save_input.name = 'usr_input'
+	ue_save_input.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	hbox_setting_l5.add_child(ue_save_input)
+	
+	var iabout:Button = Button.new()
+	iabout.name = 'iabout'
+	iabout.text = '关于'
+	iabout.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	hbox_setting_l6.add_child(iabout)
+	
+	var line2:Line2D = Line2D.new()
+	line2.name = 'line2'
+	line2.add_point(Vector2i(0, 0))
+	line2.add_point(Vector2i(win_size.x, 0))
+	hbox_setting_le.add_child(line2)
+	
+	
+	
+	
+	
+	
+	
+		
 	### L2
 	var opt_bt:OptionButton = OptionButton.new()
 	opt_bt.name = 'show_type'
@@ -124,7 +413,7 @@ func build_gui() -> void:
 	vbox_l3_vbox.name = 'vbox_l3_vbox'
 	vbox_l3.add_child(scroll_container)
 	scroll_container.add_child(vbox_l3_vbox)
-	scroll_container.custom_minimum_size.y = win_size.y - 100 - hbox_l1.size.y - hbox_l2.size.y
+	scroll_container.custom_minimum_size.y = win_size.y - 20 - hbox_l1.size.y - hbox_l2.size.y
 	
 func add_one_block(idx:int, timek:String, block_dic:Dictionary) -> void:
 	print('[connect_home]->add_one_block')
@@ -149,7 +438,7 @@ func add_one_block(idx:int, timek:String, block_dic:Dictionary) -> void:
 		texture_rec.name = "texture_rec"
 		var texture_label:Label = Label.new()
 		texture_label.name = 'texture_label'
-		var show_name_list:Array = wrap_txt("%s   %s"%[filename, filesize], 20)
+		var show_name_list:Array = wrap_txt("%s   %.1fMb"%[filename, filesize], 20)
 		if len(show_name_list) > 3:
 			texture_label.text = "%s\n%s\n%s"%[show_name_list[0], '... ...', show_name_list[2]]
 		else:
@@ -250,7 +539,48 @@ func _ts_to_month_str(ts:int) -> String:
 	var dt:Dictionary = Time.get_datetime_dict_from_unix_time(ts)
 	return "%04d-%02d"%[dt['year'], dt['month']]
 	
+func _on_login_bt_pressed() -> void:
+	print('[connect_home]->_on_login_bt_pressed')
+	vbox_l1_1_login.visible = not vbox_l1_1_login.visible
 	
+func _on_setting_bt_pressed() -> void:
+	print('[connect_home]->_on_setting_bt_pressed')
+	vbox_l1_2_setting.visible = not vbox_l1_2_setting.visible
+
+func _on_save_cfg_bt_pressed(login_title_label:Label, save_cfg_bt:Button, server_ip_input:LineEdit, 
+upload_port_input:LineEdit, download_port_input:LineEdit, usr_input:LineEdit, psd_input:LineEdit):
+	print('[connect_home]->_on_save_cfg_bt_pressed')
+	save_cfg_bt.text = '保存中... ...'
+	SERVER_IP = server_ip_input.text
+	UPLOAD_PORT = int(upload_port_input.text)
+	DOWNLOAD_PORT = int(download_port_input.text)
+	USR = usr_input.text
+	PSD = psd_input.text
+	save_cfg()
+	save_cfg_bt.text = '保存配置'
+
+func _on_test_bt_pressed(login_title_label:Label, test_bt:Button, server_ip_input:LineEdit, 
+upload_port_input:LineEdit, download_port_input:LineEdit, usr_input:LineEdit, psd_input:LineEdit):
+	print('[connect_home]->_on_test_bt_pressed')
+	test_bt.text = '测试中... ...'
+	var _SERVER_IP:String = server_ip_input.text
+	var _UPLOAD_PORT:int = int(upload_port_input.text)
+	var _DOWNLOAD_PORT:int = int(download_port_input.text)
+	var _USR:String = usr_input.text
+	var _PSD:String = psd_input.text
+	var taskid:String = generate_task_id()
+	upload_obj = TCP_TRANSF_C.new(taskid, UE_ROOT_DIR, _SERVER_IP, _UPLOAD_PORT, _USR, _PSD, 3, 'no')
+	upload_obj.connect_to_server()
+	var r:bool = upload_obj.login_do()
+	if r:
+		login_title_label.text = '登录成功'
+		login_title_label.label_settings = label_setting_font_blue
+	else:
+		login_title_label.text = '登录失败'
+		login_title_label.label_settings = label_setting_font_red
+	test_bt.text = '试连接'
+	upload_obj.disconnect_to_server()
+
 ################################# for functions ##############################
 
 func query_files() -> void:
